29ff53b04f2f838963236719963d889c3ee26824
Arjun Veer Chadha <arjun@ideashacks.com>
2021-03-09 13:18:17 +0530

    Create owlcarousel2.js

---
+++ js/owlcarousel2.js
@@ -0,0 +1,22 @@
+HTML CSS JSResult Skip Results Iframe
+// Used bootstrap v4.5,jquery v3.5.1, owl carousel v2, font awesome v4.7.0
+
+$('.testi.owl-carousel').owlCarousel({
+  items: 2,
+  margin:10,
+  lazyLoad: true,
+  dots:true,
+  autoPlay: true,
+  autoPlayTimeout: 3000,
+  responsive:{
+    0:{
+      items:1,
+    },
+    600:{
+      items:2,
+    },
+    1000:{
+      items:2,
+    }
+  }
+});
